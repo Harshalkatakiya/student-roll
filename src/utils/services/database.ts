95618e6ab6f1a2cca7cba1a69e4f6951e27a9680
Harshalkatakiya <katakiyaharshl001@gmail.com>
2024-12-22 18:17:25 +0530

    database connection updated

--- src/utils/services/database.ts
+++ src/utils/services/database.ts
@@ -1,14 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose, { Connection } from 'mongoose';
 import { DATABASE_URL } from '../constants/app.constant';
+
+let cachedConnection: Connection | null = null;
 
 export const connectToDatabase = async () => {
   try {
-    await mongoose.connect(DATABASE_URL);
-    console.log('✅ Database Connected Successfully');
+    if (cachedConnection) {
+      return cachedConnection;
+    }
+    const cnx = await mongoose.connect(DATABASE_URL);
+    cachedConnection = cnx.connection;
+    return cachedConnection;
   } catch (error) {
     console.error('💥 Error connecting to the database : ', error);
   }
 };
+
 export const disconnectFromDatabase = async () => {
   try {
     await mongoose.disconnect();
